9a26345047f7ffd5e5074cf7924abee89d43ef49
jackdebree <jdebree98@hotmail.com>
2025-06-16 11:05:29 +0200

    generated springboot project

---
+++ backend/intune-cms/src/main/java/com/intune/cms/intune_cms/IntuneCmsApplication.java
@@ -0,0 +1,13 @@
+package com.intune.cms.intune_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntuneCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntuneCmsApplication.class, args);
+	}
+
+}
---
+++ backend/intune-cms/src/test/java/com/intune/cms/intune_cms/IntuneCmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intune.cms.intune_cms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntuneCmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
